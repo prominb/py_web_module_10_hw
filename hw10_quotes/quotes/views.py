b81ccf5198da96474fdba25e953f7b0412d4b108
Roman M <prominb@gmail.com>
2024-05-07 05:11:18 +0300

    add utils-py and edit view e007

--- hw10_quotes/quotes/views.py
+++ hw10_quotes/quotes/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .utils import get_mongodb
+
 # Create your views here.
 def main(request):
-    return render(request, 'quotes/index.html', {})
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    return render(request, 'quotes/index.html', {'quotes': quotes})
