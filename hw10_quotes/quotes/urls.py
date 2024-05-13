c79a38a9565d2177763763fe3c22f494c0333d99
Roman M <prominb@gmail.com>
2024-05-13 04:01:53 +0300

    some changes e023

--- hw10_quotes/quotes/urls.py
+++ hw10_quotes/quotes/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from . import views
 
 
@@ -7,6 +8,7 @@ app_name = 'quotes'
 urlpatterns = [
     path('', views.main, name='main'),
     path('<int:page>', views.main, name='main_paginate'),
-    path('tag/<str:tag>/', views.QuotesByTagView.as_view(), name='quotes_by_tag'),
     path('author/<str:author>/', views.AuthorView.as_view(), name='author'),
+    path('tag/<str:tag>/', views.QuotesByTagView.as_view(), name='quotes_by_tag'),
+    # path('tag/<int:pk>/', views.QuotesByTagView.as_view(), name='quotes_by_tag'),
 ]
