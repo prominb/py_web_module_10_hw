796131e781e78918e672eaa31efc8ceca781f72c
Roman M <prominb@gmail.com>
2024-05-13 07:01:38 +0300

    e104 some little changes

--- hw10_quotes/hw10_quotes/settings.py
+++ hw10_quotes/hw10_quotes/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'hw10_quotes.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db_2.sqlite3',
     }
 }
 
--- hw10_quotes/quotes/views.py
+++ hw10_quotes/quotes/views.py
@@ -2,11 +2,15 @@ from django.shortcuts import render
 from django.core.paginator import Paginator
 
 from .utils import get_mongodb
+# from .models import Quote
 
 # Create your views here.
 def main(request, page=1):
     db = get_mongodb()
     quotes = db.quotes.find()
+    # quotes = Quote.objects.all()  # Exception Type: TypeError
+# Exception Value: id must be an instance of (bytes, str, ObjectId), not <class 'quotes.models.Author'>
+# 10 <span>by <small class="author" itemprop="author">{{ quote.author|author }}</small>
     per_page = 10
     paginator = Paginator(list(quotes), per_page)
     quotes_on_page = paginator.page(page)
