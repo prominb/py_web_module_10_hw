ad21dd61106417a84e2ce24b157eac4b474eb109
Roman_M <prominb@gmail.com>
2024-05-14 13:24:00 +0300

    e111 NEW START => edit urls.py

--- hw10_quotes/quotes/urls.py
+++ hw10_quotes/quotes/urls.py
@@ -6,12 +6,7 @@ app_name = 'quotes'
 
 urlpatterns = [
     path('', views.main, name='main'),  # AAAAA
-    # path('tag/', views.tag, name='tag'),
-    # path('note/', views.note, name='note'),
-    # path('detail/<int:note_id>/', views.detail, name='detail'),
-    # path('done/<int:note_id>', views.set_done, name='set_done'),
     path('<int:page>', views.main, name='main_paginate'),  # AAAAA
-    # path('', views.index, name='index'),
-    # # ex: /polls/5/
-    # path('<int:quotes_id>/', views.detail, name='detail'),
+    # NEW START => BEGIN
+    path('author/<str:id_>/', views.author_details, name='author_details'),
 ]
