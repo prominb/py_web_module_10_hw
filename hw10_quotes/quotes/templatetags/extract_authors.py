5643e77f3b9a3f4ce58db1fb2476593b867aff87
Roman M <prominb@gmail.com>
2024-05-14 05:19:45 +0300

    e107

--- hw10_quotes/quotes/templatetags/extract_authors.py
+++ hw10_quotes/quotes/templatetags/extract_authors.py
@@ -14,10 +14,11 @@ def get_author(id_):
     # author = db.authors.find_one({'_id': ObjectId(id_)})
     # return author['fullname']
 
-    author = Author.objects.get(id=id_)
+    author = Author.objects.get(id=id_)  # TypeError: Field 'id' expected a number but got <Author: Albert Einstein>.
     # author = Author.objects.get(pk=id_)
     # author = Author.objects
-    return author['fullname']
+    # return author['fullname']
+    return author
 
 
 register.filter('author', get_author)
