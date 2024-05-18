1028b6826b665e0aa839b3a705a89d97e0d04348
Roman M <prominb@gmail.com>
2024-05-19 02:48:29 +0300

    e117 ReWrite Users

--- hw10_quotes/users/views.py
+++ hw10_quotes/users/views.py
@@ -1,35 +1,54 @@
-from django.shortcuts import render,redirect
-from django.views import View
 from django.contrib import messages
-from django.urls import reverse_lazy
-from django.contrib.auth.views import PasswordResetView
-from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
-from .forms import RegisterForm
+from .forms import RegisterForm, LoginForm, ProfileForm
+
 
 # Create your views here.
-class RegisterView(View):
-    form_class = RegisterForm
-    template_name = "users/signup.html"
+def signupuser(request):
+    if request.user.is_authenticated:
+        return redirect(to='noteapp:main')
 
-    def get(self, request):
-        return render(request, self.template_name, {"form": self.form_class})
-
-    def post(self, request):
-        form = self.form_class(request.POST)
-        print(form)
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data["username"]
-            messages.success(request, f'Вітаємо, {username}! Ваш акаунт успішно створений')
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'users/signup.html', context={"form": form})
+
+    return render(request, 'users/signup.html', context={"form": RegisterForm()})
+
+def loginuser(request):
+    if request.user.is_authenticated:
+       return redirect(to='noteapp:main')
+
+    if request.method == 'POST':
+        user = authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            messages.error(request, 'Username or password didn\'t match')
             return redirect(to='users:login')
-        return render(request, self.template_name, {"form": form})
 
+        login(request, user)
+        return redirect(to='noteapp:main')
 
-class ResetPasswordView(SuccessMessageMixin, PasswordResetView):
-    template_name = 'users/password_reset.html'
-    email_template_name = 'users/password_reset_email.html'
-    html_email_template_name = 'users/password_reset_email.html'
-    success_url = reverse_lazy('users:password_reset_done')
-    success_message = "An email with instructions to reset your password has been sent to %(email)s."
-    subject_template_name = 'users/password_reset_subject.txt'
+    return render(request, 'users/login.html', context={"form": LoginForm()})
+
+@login_required
+def logoutuser(request):
+    logout(request)
+    return redirect(to='noteapp:main')
+
+@login_required
+def profile(request):
+    if request.method == 'POST':
+        profile_form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if profile_form.is_valid():
+            profile_form.save()
+            messages.success(request, 'Your profile is updated successfully')
+            return redirect(to='users:profile')
+
+    profile_form = ProfileForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'profile_form': profile_form})
